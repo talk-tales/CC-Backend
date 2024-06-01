89e5dbec130b7dcd0c9dc8158be8cb44a715e54a
TalkTales-C241-PS275 <168517742+talk-tales@users.noreply.github.com>
2024-06-01 17:36:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authRoutes = require("./routes/auth");
 const authMiddleware = require("./middleware/auth.middleware");
 require("dotenv").config();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
