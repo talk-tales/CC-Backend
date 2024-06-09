9399c5aac264a1fc493265a9c1d5e6c1bf08d936
dandi lesmana <dandilesmana.if21@gmail.com>
2024-06-09 22:10:34 +0700

    refactor: refactor code get story for optimized performance

--- controller/storyController.js
+++ controller/storyController.js
@@ -1,17 +1,32 @@
 const prisma = require("../lib/prisma");
 
 const getAllStory = async () => {
-    return await prisma.story.findMany();
+    return await prisma.story.findMany({
+        select: {
+            id: true,
+            title: true,
+            thumbnail_url: true,
+            city: true,
+        }
+    })
 };
 
 const getStoryById = async (storyId) => {
     return await prisma.story.findUnique({
+   
         where: {
             id: parseInt(storyId),
         },
-        include: {
+        select: {
+            id: true,
+            title: true,
+            synopsis: true,
+            prolog_image_url: true,
+            city: true,
+            conversation_count: true,
             Glosarium: true,
-        }
+        },
+
     });
 };
 
